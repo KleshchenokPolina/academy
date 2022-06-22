be45cfbda3007ad1e6d305946c72f8bf8a2eda8b
Polina <kleshchenokpolina@gmail.com>
2022-06-22 22:02:19 +0300

    hello

---
+++ src/main/java/by/academy/classwork/lesson3/Task3.java
@@ -0,0 +1,13 @@
+package by.academy.classwork.lesson3;
+
+public class Task3 {
+    public static void main (String [] arg){
+        int x = 11;
+        if (x%2==0){
+            System.out.println ("Numer is even.");
+        }
+        else {
+            System.out.println("Numer is odd.");
+        }
+    }
+}
